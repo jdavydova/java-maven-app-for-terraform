454b08d094ef9cd4a730ef52ef8bd1dea38ac884
Julia Davydova <juliada888@gmail.com>
2026-04-25 12:21:01 +0300

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable vpc_cidr_block {
+  default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+  default = "10.0.10.0/24"
+}
+variable avail_zone {
+  default = "eu-north-1"
+}
+variable env_prefix {
+  default = "dev"
+}
+variable my_ip {
+  default = "91.138.187.78"
+}
+variable jenkins_ip {
+  default = "164.92.182.1/32"
+}
+variable instance_type {
+  default = "t2.micro"
+}
+variable region {
+  default = "eu-north-1"
+}
